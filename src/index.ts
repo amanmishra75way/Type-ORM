b2a1e11649cd84b59a94e0d9eb96928578ba39bc
amanmishra75way <amishra.75way@gmail.com>
2025-06-30 12:53:41 +0530

    Refactor index.ts: remove student insertion logic and streamline student retrieval

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,9 @@ import { User } from "./entity/User";
 import { Students } from "./entity/Students";
 AppDataSource.initialize()
   .then(async () => {
-    console.log("Inserting a new student into the database...");
-    const newStudent = new Students();
-    newStudent.name = "Aman";
-    newStudent.age = 21;
-    newStudent.city = "Noida";
-    newStudent.weight = 65;
-    await AppDataSource.manager.save(newStudent);
-    console.log("Saved a new studnet with " + newStudent.id);
+    const studentRepository = AppDataSource.getRepository(Students);
 
-    console.log("Loading students from the database...");
-    const students = await AppDataSource.manager.find(Students);
-    console.log("Loaded Students: ", students);
-
-    console.log("Here you can setup and run express / fastify / any other framework.");
+    const allStudent = await studentRepository.find();
+    console.log("STUDENTS DETIALS ARE", allStudent);
   })
   .catch((error) => console.log(error));
