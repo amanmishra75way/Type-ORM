8bdc51279fa2e2ff751af8fc36f11c3665a348fd
amanmishra75way <amishra.75way@gmail.com>
2025-06-30 12:55:34 +0530

    Refactor index.ts: change student retrieval to delete all students

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ AppDataSource.initialize()
   .then(async () => {
     const studentRepository = AppDataSource.getRepository(Students);
 
-    const allStudent = await studentRepository.find();
+    const allStudent = await studentRepository.deleteAll();
     console.log("STUDENTS DETIALS ARE", allStudent);
   })
   .catch((error) => console.log(error));
